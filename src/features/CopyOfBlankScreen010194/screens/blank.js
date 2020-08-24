92ad68e503980142b12a933e21817c52750873e8
Crowdbotics <team@crowdbotics.com>
2020-08-24 05:56:40 +0000

    Update components

--- src/features/CopyOfBlankScreen010194/screens/blank.js
+++ src/features/CopyOfBlankScreen010194/screens/blank.js
@@ -128,36 +128,6 @@ export class _Blank extends React.Component {
       >
         Press me!
       </Button>
-      <Image
-        resizeMode="cover"
-        source={{
-          uri:
-            "https://d3nfddh2sbo9hk.cloudfront.net/media/project_component_resources/7_S7n0vw0.jpg"
-        }}
-        style={{
-          width: "100%",
-          height: 200,
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 0,
-          marginBottom: 0,
-          paddingLeft: 0,
-          paddingRight: 0,
-          paddingTop: 0,
-          paddingBottom: 0,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0
-        }}
-      />
       <Datepicker
         style={{
           width: "100%",
@@ -240,6 +210,36 @@ export class _Blank extends React.Component {
           paddingRight: 5,
           paddingTop: 5,
           paddingBottom: 5,
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0
+        }}
+      />
+      <Image
+        resizeMode="cover"
+        source={{
+          uri:
+            "https://d3nfddh2sbo9hk.cloudfront.net/media/project_component_resources/7_S7n0vw0.jpg"
+        }}
+        style={{
+          width: "100%",
+          height: 200,
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 0,
+          marginBottom: 0,
+          paddingLeft: 0,
+          paddingRight: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
           overflow: "visible",
           textAlign: "left",
           verticalAlign: "baseline",
