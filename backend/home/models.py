853ef0d04471aaf0fcac6f3a2bc93c59f6e2dbea
Crowdbotics <team@crowdbotics.com>
2020-08-24 05:58:06 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,27 +7,59 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150,)
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
 
 
 class HomePage(models.Model):
     body = models.TextField()
+    key1 = models.ForeignKey(
+        "home.CustomText",
+        on_delete=models.SET_DEFAULT,
+        default=1,
+        null=True,
+        blank=True,
+        related_name="homepage_key1",
+    )
+    key2 = models.ForeignKey(
+        "home.CustomText",
+        on_delete=models.SET_DEFAULT,
+        default=2,
+        null=True,
+        blank=True,
+        related_name="homepage_key2",
+    )
+    key3 = models.OneToOneField(
+        "users.User",
+        on_delete=models.SET_DEFAULT,
+        default=3,
+        null=True,
+        blank=True,
+        related_name="homepage_key3",
+    )
+    key4 = models.OneToOneField(
+        "users.User",
+        on_delete=models.SET_DEFAULT,
+        default=4,
+        null=True,
+        blank=True,
+        related_name="homepage_key4",
+    )
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
